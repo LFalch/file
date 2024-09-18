e0a088f8da2ae069940305f3834c24784075ae7e
LFalch <lucas@wasd.dk>
2024-09-18 16:40:00 +0200

    Add UTF-16 and fix UTF-8

--- src/file.zig
+++ src/file.zig
@@ -5,6 +5,8 @@ pub const Class = enum {
     ascii,
     latin1,
     utf8,
+    utf16_le,
+    utf16_be,
     data,
 
     pub fn name(self: @This()) []const u8 {
@@ -13,6 +15,8 @@ pub const Class = enum {
             .ascii => "ASCII text",
             .latin1 => "ISO-8859 text",
             .utf8 => "UTF-8 Unicode text",
+            .utf16_le => "Little-endian UTF-16 Unicode text",
+            .utf16_be => "Big-endian UTF-16 Unicode text",
             .data => "data",
         };
     }
@@ -26,16 +30,33 @@ fn isAsciiText(byte: u8) bool {
         else => false,
     };
 }
+fn codepoint(low_surrogate: u16, high_surrogate: u16) u21 {
+    const low_half: u21 = low_surrogate;
+    const high_half: u21 = high_surrogate;
+    return 0x10000 + ((high_half & 0x03ff) << 10) | (low_half & 0x03ff);
+}
+const MAX_CODEPOINT: u21 = 0x10FFFF;
+const BOM_LE: [2]u8 = [_]u8{ 0xff, 0xfe };
+const BOM_BE: [2]u8 = [_]u8{ 0xfe, 0xff };
 
 pub const Classifier = struct {
-    const State = packed struct(u4) {
+    const State = packed struct(u6) {
         empty: bool = true,
         ascii: bool = true,
         latin1: bool = true,
         utf8: bool = true,
+        utf16: enum(u2) {
+            none = 0,
+            le,
+            be,
+            no_bom_yet,
+        } = .no_bom_yet,
     };
     possible_text: State = .{},
     expected_follow_bytes: u2 = 0,
+
+    first_utf16_byte: ?u8 = null,
+    first_surrogate: ?u16 = null,
 
     const Self = @This();
 
@@ -58,10 +79,11 @@ pub const Classifier = struct {
                     // if it isn't a follow byte, it is not UTF-8
                     if (@clz(~byte) != 1) self.possible_text.utf8 = false;
                 } else {
-                    const follow_bytes = std.unicode.utf8ByteSequenceLength(byte) catch {
+                    const seq_length = std.unicode.utf8ByteSequenceLength(byte) catch {
                         self.possible_text.utf8 = false;
                         break :utf8;
-                    } - 1;
+                    };
+                    const follow_bytes = seq_length - 1;
 
                     if (follow_bytes == 0) {
                         if (!isAsciiText(byte)) self.possible_text.utf8 = false;
@@ -72,11 +94,52 @@ pub const Classifier = struct {
             }
         }
 
+        utf16: {
+            if (self.first_utf16_byte) |prev_byte| {
+                self.first_utf16_byte = null;
+                const u8_u8 = [2]u8{ prev_byte, byte };
+
+                const endian: std.builtin.Endian = switch (self.possible_text.utf16) {
+                    .no_bom_yet => {
+                        self.possible_text.utf16 = if (std.mem.eql(u8, &u8_u8, &BOM_LE)) .le else if (std.mem.eql(u8, &u8_u8, &BOM_BE)) .be else .none;
+                        break :utf16;
+                    },
+                    .none => break :utf16,
+                    .le => .little,
+                    .be => .big,
+                };
+
+                const code_unit = std.mem.readInt(u16, &u8_u8, endian);
+
+                if (self.first_surrogate) |high_surrogate| {
+                    self.first_surrogate = null;
+                    if (!std.unicode.utf16IsLowSurrogate(code_unit) or codepoint(code_unit, high_surrogate) > MAX_CODEPOINT)
+                        self.possible_text.utf16 = .none;
+                } else if (std.unicode.utf16IsHighSurrogate(code_unit)) {
+                    self.first_surrogate = code_unit;
+                } else {
+                    // the code unit is the whole code point
+                    if (!(code_unit >= 160 or isAsciiText(@intCast(code_unit))))
+                        self.possible_text.utf16 = .none;
+                }
+            } else {
+                self.first_utf16_byte = byte;
+            }
+        }
+
         // say we're done if they are no possible classifications
-        return @as(u4, @bitCast(self.possible_text)) == 0;
+        return @as(u6, @bitCast(self.possible_text)) == 0;
     }
     pub fn finish(self: Self) Class {
         if (self.possible_text.empty) return .empty;
+
+        if (self.first_utf16_byte == null and self.first_surrogate == null) {
+            switch (self.possible_text.utf16) {
+                .le => return .utf16_le,
+                .be => return .utf16_be,
+                else => {},
+            }
+        }
 
         if (self.possible_text.ascii) return .ascii;
         if (self.possible_text.utf8 and self.expected_follow_bytes == 0) return .utf8;
